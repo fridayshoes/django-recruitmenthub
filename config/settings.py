2973597b60948ace3e8f15e8c1a1adada235d197
fridayshoes <one_amp@hotmail.com>
2026-03-09 10:39:04 +0000

    setup initial app features

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trainer',
+    'institute',
+    'company',
 ]
 
 MIDDLEWARE = [
